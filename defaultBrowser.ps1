41a1ca8f0b164c4068faa66f6c16e1e2bb06ba9a
ormeta <102360109+ormeta@users.noreply.github.com>
2022-10-11 11:41:18 +0200

    Update defaultBrowser.ps1

--- defaultBrowser.ps1
+++ defaultBrowser.ps1
@@ -9,30 +9,35 @@ Start-Process -FilePath "control"
 Start-Sleep 1
 for ($i = 0; $i -lt 12; $i++) {
     $wshell.SendKeys("{TAB}")
+    Start-sleep 0.5
 }
 $wshell.SendKeys("{ENTER}")
 
 Start-Sleep 1
 for ($i = 0; $i -lt 6; $i++) {
     $wshell.SendKeys("{TAB}")
+    Start-sleep 0.5
 }
 $wshell.SendKeys("{ENTER}")
 
 Start-Sleep 1
 for ($i = 0; $i -lt 3; $i++) {
     $wshell.SendKeys("{TAB}")
+    Start-sleep 0.5
 }
 $wshell.SendKeys("{ENTER}")
 
 Start-Sleep 1
 for ($i = 0; $i -lt 5; $i++) {
     $wshell.SendKeys("{TAB}")
+    Start-sleep 0.5
 }
 $wshell.SendKeys("{ENTER}")
 
 Start-Sleep 1
 for ($i = 0; $i -lt 1; $i++) {
     $wshell.SendKeys("{TAB}")
+    Start-sleep 0.5
 }
 $wshell.SendKeys("{ENTER}")
 
