d42356e2e2726cc38b39aa3fe71c0198c927040f
ormeta <102360109+ormeta@users.noreply.github.com>
2022-10-11 09:33:50 +0200

    Add files via upload

---
+++ defaultBrowser.ps1
@@ -0,0 +1,48 @@
+
+
+
+#SendKeys
+$wshell = New-Object -ComObject wscript.shell 
+
+Start-Process -FilePath "control"
+
+Start-Sleep 1
+for ($i = 0; $i -lt 12; $i++) {
+    $wshell.SendKeys("{TAB}")
+}
+$wshell.SendKeys("{ENTER}")
+
+Start-Sleep 1
+for ($i = 0; $i -lt 6; $i++) {
+    $wshell.SendKeys("{TAB}")
+}
+$wshell.SendKeys("{ENTER}")
+
+Start-Sleep 1
+for ($i = 0; $i -lt 3; $i++) {
+    $wshell.SendKeys("{TAB}")
+}
+$wshell.SendKeys("{ENTER}")
+
+Start-Sleep 1
+for ($i = 0; $i -lt 5; $i++) {
+    $wshell.SendKeys("{TAB}")
+}
+$wshell.SendKeys("{ENTER}")
+
+Start-Sleep 1
+for ($i = 0; $i -lt 1; $i++) {
+    $wshell.SendKeys("{TAB}")
+}
+$wshell.SendKeys("{ENTER}")
+
+Start-Sleep 3
+$wshell.SendKeys("%{F4}")
+Start-Sleep 1
+$wshell.SendKeys("%{F4}")
+
+
+
+
+
+
